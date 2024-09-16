09fb662f54975d3a499a658385e495e44167fe6b
mtuszynski <tuszynski.mir@gmail.com>
2024-09-16 12:37:45 +0200

    feat: add table creation during plugin activation

--- includes/class-subscribe-to-pdf-activator.php
+++ includes/class-subscribe-to-pdf-activator.php
@@ -20,7 +20,8 @@
  * @subpackage Subscribe_To_Pdf/includes
  * @author     MirT <office@mirt.pl>
  */
-class Subscribe_To_Pdf_Activator {
+class Subscribe_To_Pdf_Activator
+{
 
 	/**
 	 * Short Description. (use period)
@@ -29,8 +30,35 @@ class Subscribe_To_Pdf_Activator {
 	 *
 	 * @since    1.0.0
 	 */
-	public static function activate() {
+	public static function activate()
+	{
+		self::create_table('subscribe_for_pdf_subscribers', "
+            id mediumint(9) NOT NULL AUTO_INCREMENT,
+            first_name varchar(55) NOT NULL,
+            last_name varchar(55) NOT NULL,
+            email varchar(255) NOT NULL,
+            submission_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY  (id),
+            INDEX email_index (email)
+        ");
 
+		self::create_table('subscribe_for_pdf_settings', "
+            id mediumint(9) NOT NULL AUTO_INCREMENT,
+            document_url varchar(255) NOT NULL,
+			delete_tables_on_deactivation tinyint(1) DEFAULT 0,
+            PRIMARY KEY  (id)
+        ");
 	}
+	private static function create_table($table_suffix, $sql)
+	{
+		global $wpdb;
 
+		$table_name = $wpdb->prefix . $table_suffix;
+		$charset_collate = $wpdb->get_charset_collate();
+
+		$sql = "CREATE TABLE $table_name ($sql) $charset_collate;";
+
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($sql);
+	}
 }
