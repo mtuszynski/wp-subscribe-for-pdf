1b3ab840ed19a1ddb4d47106af58983f7af03618
mtuszynski <tuszynski.mir@gmail.com>
2024-09-16 12:43:24 +0200

    feat: add table deletion logic on plugin deactivation - Implemented logic to check if tables should be deleted based on 'delete_tables_on_deactivation' setting.

--- includes/class-subscribe-to-pdf-deactivator.php
+++ includes/class-subscribe-to-pdf-deactivator.php
@@ -20,7 +20,8 @@
  * @subpackage Subscribe_To_Pdf/includes
  * @author     MirT <office@mirt.pl>
  */
-class Subscribe_To_Pdf_Deactivator {
+class Subscribe_To_Pdf_Deactivator
+{
 
 	/**
 	 * Short Description. (use period)
@@ -29,8 +30,47 @@ class Subscribe_To_Pdf_Deactivator {
 	 *
 	 * @since    1.0.0
 	 */
-	public static function deactivate() {
-
+	public static function deactivate()
+	{
+		if (self::should_delete_tables()) {
+			self::drop_table('subscribe_for_pdf_subscribers');
+			self::drop_table('subscribe_for_pdf_settings');
+		}
 	}
 
+	/**
+	 * Checks whether database tables should be deleted upon plugin deactivation.
+	 *
+	 * This function queries the 'subscribe_for_pdf_settings' table to determine
+	 * if the tables should be removed when the plugin is deactivated. It looks 
+	 * for the value in the 'delete_tables_on_deactivation' column for the record 
+	 * with an 'id' of 1.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @global wpdb $wpdb WordPress database abstraction object.
+	 *
+	 * @return bool True if tables should be deleted, false otherwise.
+	 */
+	private static function should_delete_tables()
+	{
+		global $wpdb;
+		$table_name = $wpdb->prefix . 'subscribe_for_pdf_settings';
+		$query = $wpdb->prepare("SELECT delete_tables_on_deactivation FROM $table_name WHERE id = %d", 1);
+		$result = $wpdb->get_var($query);
+		return (bool) $result;
+	}
+
+	/**
+	 * Drop a table from the database.
+	 *
+	 * @since    1.0.0
+	 */
+	private static function drop_table($table_suffix)
+	{
+		global $wpdb;
+		$table_name = $wpdb->prefix . $table_suffix;
+		$sql = "DROP TABLE IF EXISTS $table_name;";
+		$wpdb->query($sql);
+	}
 }
